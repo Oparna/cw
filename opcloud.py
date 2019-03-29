9300aca6227cd61ad37db7b94373ca138360c178
Oparna <48723785+Oparna@users.noreply.github.com>
2019-03-29 21:14:34 +0000

    Update opcloud.py

--- opcloud.py
+++ opcloud.py
@@ -15,6 +15,7 @@ url_name = 'https://restcountries.eu/rest/v2/name/{name}'
 url_callingcode = 'https://restcountries.eu/rest/v2/callingcode/{callingcode}'
 url_city = 'https://restcountries.eu/rest/v2/capital/{capital}'
 
+# This route is the initial route that the user goes to login and enter the homepage.  
 @app.route('/', methods=['GET', 'POST'])
 def login():
 	error = None
@@ -30,16 +31,21 @@ def login():
 def home():
 	return render_template('homepage.html')
 
+#Method to specify whether it is POST or GET. This allows you to enter a country name. If it doesnt exist then error code appears
 @app.route('/name',  methods=['GET', 'POST'])
 def name_country():
+	# This is to get the data, in this case to get the country name entered
     url = url_name.format(name=request.form.get('name'))
     response = requests.get(url)
+	# If it doesn't exists then return the following text, 404 specifies which error code.
     if response.status_code == 404:
         return "<h1>Error, page does not exist!</h1>", 404
     response = response.json()
     return jsonify(response)
     #return render_template('backg.html', result = response)
-
+	
+#Method to specify whether it is POST or GET. This allows you to enter a calling code. If it doesnt exist then error code appears (404). 
+#The return is a json format, although a html format is available but hashed out currently. 
 @app.route('/callingcode',  methods=['GET', 'POST'])
 def callcode():
     url = url_callingcode.format(callingcode=request.form.get('callingcode'))
@@ -49,7 +55,8 @@ def callcode():
     response = response.json()
     return jsonify(response)
     #return render_template('backg.html', result = response, par="Code {}".format(request.form.get('callingcode')))
-
+	
+#Method to specify whether it is POST or GET. This allows you to enter a capital city. If it doesnt exist then error code appears (404)
 @app.route('/city',  methods=['GET', 'POST'])
 def capcity():
 	url = url_city.format(capital=request.form.get('city'))
@@ -61,6 +68,6 @@ def capcity():
 	#return render_template('backg.html', result = response, par="cityname {}".format(request.form.get('city')))
 
 
-
+# In order to run the app this is needed
 if __name__=="__main__":
     app.run(port=8080, debug=True)
